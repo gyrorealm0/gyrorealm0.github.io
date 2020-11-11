d8df5fdfd839260d9529225010ab22bf6bffd2f6
gyrorealm0 <63954584+gyrorealm0@users.noreply.github.com>
2020-11-10 17:01:56 -0900

    poggies

--- script.js
+++ script.js
@@ -3,7 +3,16 @@ var playerData = [
     currencyMakers = 0,
     currencyMakerCost = 10,
     prestigePoints = 0,
-    possiblePrestigePoints = 0
+    possiblePrestigePoints = 0,
+    currencyUpgrades = 1,
+    prestigeUpgrades = 1,
+    currencyUpgradeCost = 10,
+    prestigeUpgradeCost = 1000,
+    currencyMultiplier = 1,
+    prestigeMultiplier = 1,
+    upgradeUpgrade = 1,
+    upgradeUpgradeCost = 2,
+    upgradeMultiplier = 1
 ]
 
 function fixVar() {
@@ -17,6 +26,10 @@ function updateVar() {
     document.getElementById("currencyMakerCost").innerHTML = currencyMakerCost;
     document.getElementById("prestigePoints").innerHTML = prestigePoints.toFixed(2);
     document.getElementById("possiblePrestigePoints").innerHTML = possiblePrestigePoints.toFixed(2);
+    document.getElementById("currencyUpgradeCost").innerHTML = currencyUpgradeCost;
+    document.getElementById("prestigeUpgradeCost").innerHTML = prestigeUpgradeCost;
+    document.getElementById("upgradeUpgradeCost").innerHTML = upgradeUpgradeCost;
+    
 }
 
 function save() {
@@ -29,7 +42,7 @@ function getCurrency() {
 }
 
 function buyCurrencyMaker() {
-    while(currency > currencyMakerCost) {
+    while(currency >= currencyMakerCost) {
         if (currency >= currencyMakerCost) {
         currency = currency - currencyMakerCost;
         currencyMakers++;
@@ -47,15 +60,15 @@ function updateGame() {
 }
 
 function prestigeCheck() {
-    possiblePrestigePoints = Math.sqrt(currency + currencyMakers) / (1 + prestigePoints);
+    possiblePrestigePoints = (Math.sqrt(currency + currencyMakers) / (1 + prestigePoints)) * (prestigeMultiplier) * (upgradeMultiplier);
 }
 
 function makeCurrency() {
-    currency += currencyMakers * prestigePoints / 100;
+    currency += currencyMultiplier * currencyMakers * (1 + prestigePoints) / 100;
 }
 
 function prestige() {
-    prestigePoints += Math.sqrt(currency + currencyMakers) / (1 + prestigePoints);
+    prestigePoints += Math.sqrt((currency + currencyMakers) / (1 + prestigePoints)) * (prestigeMultiplier) * (upgradeMultiplier);
     currencyMakers = 0;
     currency = 0;
     currencyMakerCost = 10;
@@ -63,6 +76,33 @@ function prestige() {
 
 function load() {
     prestigePoints = parseFloat(document.cookie);
+}
+
+function upgradePrestige() {
+    if(currency >= prestigeUpgradeCost) {
+        currenecy -= prestigeUpgradeCost;
+        prestigeUpgradeCost *= 10;
+        prestigeMultiplier *= 2;
+    }
+}
+
+function upgradeCurrency() {
+    if(prestigePoints >= currencyUpgradeCost) {
+        prestigePoints -= currencyUpgradeCost;
+        currencyUpgradeCost *= 10;
+        currencyMultiplier *= 2;
+    }
+}
+
+function upgradeUpgrade() {
+    if(currencyUpgrades >= upgradeUpgradeCost) {
+        if(prestigeUpgrades >= upgradeUpgradeCost) {
+            currencyUpgrades -= upgradeUpgradeCost;
+            prestigeUpgrades -= upgradeUpgradeCost;
+            upgradeUpgradeCost *= 5;
+            upgradeMultiplier *= 2;
+        }
+    }
 }
 
 fixVar();
