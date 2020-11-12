639bf7b77aba48a2daa014dd22f39ef98d39a1ac
gyrorealm0 <63954584+gyrorealm0@users.noreply.github.com>
2020-11-12 14:27:46 -0900

    a

---
+++ purchase.js
@@ -0,0 +1,23 @@
+function purchase(currency, cost, max) {
+    if(currency < cost) {
+        return 0;
+    }
+
+    if(max == true) {
+        return Math.floor(currency / cost);
+    }
+
+    return 1;
+}
+
+function purcahse(currency, cost, multiplier, max) {
+    if(currency < cost) {
+        return 0;
+    }
+
+    if(max == true) {
+        return Math.floor(Math.log10(currency * (multiplier - 1) / cost + 1) / Math.log10(multiplier));
+    }
+
+    return 1;
+}
