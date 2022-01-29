ce2a912b5a755c6c2a7341c6d597023042a1703d
gyrorealm0 <63954584+gyrorealm0@users.noreply.github.com>
2022-01-29 10:08:21 -0800

    Update script.js

--- linpack/script.js
+++ linpack/script.js
@@ -1,33 +1,6 @@
-document.addEventListener('dragenter', ev => {
-    ev.stopPropagation()
-    ev.preventDefault()
-})
-document.addEventListener('dragover', ev => {
-    ev.stopPropagation()
-    ev.preventDefault()
-})
-document.addEventListener('drop', async ev => {
-    ev.stopPropagation()
-    ev.preventDefault()
-    for (const file of ev.dataTransfer.files) {
-        if (fileIndex >= 13) {
-            break
-        }
-        const fileName = file.name
-        if (fileName.endsWith('.csv')) {
-            const index = ++fileIndex
-            await new Promise((resolve, reject) => {
-                Papa.parse(file, {
-                    complete: (result) => {
-                        processCSV(fileName, index, result.data)
-                        resolve()
-                    }
-                })
-            })
-        }
-        else if (fileName.endsWith('.json')) {
-            const index = ++fileIndex
-            processJSON(fileName, index, JSON.parse(await file.text()))
-        }
-    }
-})
+function dragOverHandler(ev) {
+  console.log('File(s) in drop zone');
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+}
