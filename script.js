a7f24e39ac5c86642e6d16bda7e7f39faf8aaeba
gyrorealm0 <63954584+gyrorealm0@users.noreply.github.com>
2022-01-31 02:05:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var input = [];
 var data = [];
+var results = [];
 
-var canvas = document.getElementById("canvas");
 
 async function dropHandler(ev) 
 {
@@ -33,6 +33,8 @@ function parse()
 
 	for(i = 0; i < input.length; i++)
 	{
+		log('parsing file ' + (i + 1));
+		
 		var temp = input[i].split('\r\n');
 		var size = 0;
 		var sizea = [];
@@ -42,6 +44,8 @@ function parse()
 		var avga = [];
 		var max = 0;
 		var maxa = [];
+		var time = 0;
+		var timea = [];
 		var split = 0;
 
 		for(j = 0; j < temp.length; j++)
@@ -50,10 +54,17 @@ function parse()
 
 			if(temp[j].startsWith('Trials completed:'))
 			{
-				mina.push(temp[j - split].substring(39, 47));
-				avga.push(temp[j - split].substring(53, 61));
-				maxa.push(temp[j - split].substring(67));
+				mina.push(temp[j].substring(39, 47));
+				avga.push(temp[j].substring(53, 61));
+				maxa.push(temp[j].substring(67));
 				sizea.push(size);
+				timea.push(time);
+			}
+	
+
+			if(temp[j].startsWith('Time to run:'))
+			{
+				time = temp[j].substring(13, temp[j].indexOf[" "]);
 			}
 
 			if(temp[j].startsWith('Problem size:'))
@@ -65,23 +76,26 @@ function parse()
 				data[count].push(mina);
 				data[count].push(avga);
 				data[count].push(maxa);
+				data[count].push(timea);
 				count++;
-				}
+			}
 				split = j;
 				sizea = [];
 				maxa = [];
 				avga = [];
 				mina = [];
+				timea = [];
 				j += 11;
-			}
-			
-			
 		}
+			
+			
+	}
 		data[count] = new Array();
 		data[count].push(sizea);
 		data[count].push(mina);
 		data[count].push(avga);
 		data[count].push(maxa);	
+		data[count].push(timea);
 		count++;
 	}
 
@@ -89,33 +103,103 @@ function parse()
 }
 
 function graph(){
+	var canvas = document.getElementById("canvas");
 	log('graphing started');
 	document.getElementById("image").hidden = true;
-
-	log(data.length);
+	log('analysis started');
+	for(i = 0; i < data.length; i++)
+	{
+		analyze(i);
+	}
 
 	canvas.width = window.innerWidth - 270;
 	canvas.height = window.innerHeight;
-	
-	var w = window.innerWidth - 232;
-	var h = window.innerHeight;
-	var shift = 602;
 	var ctx = canvas.getContext("2d");
-	
+
 	var image = document.getElementById("image");
 	ctx.drawImage(image, 0, 0);
-	ctx.fillStyle = 'green';
 
-	for(i = 0; i < data.length; i++)
+	var b = 974;
+	ctx.fillStyle = 'white';
+	ctx.font = '2.5vh Typewriter'
+	ctx.fillText("File", 32, 50);
+	ctx.fillText("Problem Size", 146, 50);
+	ctx.fillText("Time to Run", 362, 50);
+	ctx.fillText("Range", 582, 50);
+	ctx.fillText("Variation", 728, 50);
+	ctx.fillText("Loss", 932, 50);
+	ctx.fillText("Score", 1142, 50);
+
+	ctx.fillText("File", 32, b);
+	ctx.fillText("Problem Size", 146, b);
+	ctx.fillText("Time to Run", 362, b);
+	ctx.fillText("Range", 582, b);
+	ctx.fillText("Variation", 728, b);
+	ctx.fillText("Loss", 932, b);
+	ctx.fillText("Score", 1142, b);
+	
+	
+	
+	
+	
+		
+	ctx.font = '2vh Typewriter'
+
+	for(i = 0; i < 22; i++)
+	{
+		for(j = 0; j < 7; j++)
 		{
-			ctx.fillText(data[i][0][0], w/2, h/2 + 10 * i);
-			log(data[i][0][0]);
+			ctx.fillText(results[i][j], 32 + 175 * j, 90 + 40 * i);
 		}
+	}
+
+	
 	
 
 	log('graphing complete');
 }
 
+function analyze(i)
+{
+	log('analyzing run ' + (1 + i));
+
+		var maxi = 0;	
+		var mini = 1000;
+		var avg = 0;
+		var range = 0;
+		var variation = 0;
+		var loss = 0;
+		var score = 0;
+		var temp = 0;
+		var tempa = [];
+		var time = data[i][4][0].substring(0, data[i][4][0].indexOf(" "));
+		for(j = 0; j < data[i][0].unshift(); j++)
+		{
+			if(data[i][1][j] < mini){ mini = data[i][1][j]; } 
+			if(data[i][3][j] > maxi){ maxi = data[i][3][j]; } 
+
+			temp = temp + Number.parseFloat(data[i][2][j]);
+		}
+		avg = temp / data[i].length;
+		range = maxi - mini;
+
+		variation = 100 * (range / avg);
+		loss = data[i][2][0] - data[i][2][data[i].length];
+		score = 0.1 * (((time) / (variation)) - ((time) * (loss)));
+		
+				
+
+		tempa.push(i + 1);
+		tempa.push(data[i][0][0]);
+		tempa.push(data[i][4][0]);
+		tempa.push(parseFloat(range).toFixed(2) + " Gflops");
+		tempa.push(parseFloat(variation).toFixed(2) + " Gflops");
+		tempa.push(parseFloat(loss).toFixed(2) + " Gflops");
+		tempa.push(parseFloat(score).toFixed(2) + " Stability");
+		results.push(tempa);
+
+		log('analysis complete');
+}
 
 
 function dragOverHandler(ev) {ev.preventDefault();}
